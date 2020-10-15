741170ae7f129e9e7185d5f51ee405d562ee19d7
Mehul-Bisht <mehulbishtanna@gmail.com>
2020-10-15 22:47:01 +0530

    Added Appbar and Dark mode

---
+++ app/src/main/java/com/codinginflow/foodingo/BottomSheetDialog.kt
@@ -0,0 +1,24 @@
+package com.codinginflow.foodingo
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
+/**
+ * Created by Mehul Bisht on 15-10-2020
+ */
+class BottomSheetDialog : BottomSheetDialogFragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.bottom_sheet,container,false)
+
+        return view
+    }
+
+}
--- app/src/main/java/com/codinginflow/foodingo/ui/order/OrderFragment.kt
+++ app/src/main/java/com/codinginflow/foodingo/ui/order/OrderFragment.kt
@@ -6,7 +6,9 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.codinginflow.foodingo.BottomSheetDialog
 import com.codinginflow.foodingo.R
+import com.google.android.material.appbar.MaterialToolbar
 
 class OrderFragment : Fragment() {
 
@@ -16,17 +18,26 @@ class OrderFragment : Fragment() {
 
     private lateinit var viewModel: OrderViewModel
 
+    var toolbarRef : MaterialToolbar ? = null
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.order_fragment, container, false)
+        val view =  inflater.inflate(R.layout.order_fragment, container, false)
+        toolbarRef = view.findViewById(R.id.toolbar)
+        return view
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(OrderViewModel::class.java)
         // TODO: Use the ViewModel
+
+        toolbarRef!!.setOnClickListener {
+            val b = BottomSheetDialog()
+            b.show(requireActivity().supportFragmentManager,"bottom sheet")
+        }
     }
 
 }
